1ffbe80cf503d5fa89dde388c3e30fb7c2bcad26
Alvin Zhang <zhangxiaoting320@gmail.com>
2022-12-02 23:55:27 +0800

    complete hw2

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,78 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[] world;
+    private int worldSize;
+    private int numberOfOpened;
+    private WeightedQuickUnionUF disjointSet;
+    private int top;
+    private int bottom;
 
+    public Percolation(int N) {                // create N-by-N grid, with all sites initially blocked
+        if (N <= 0){
+            throw new IllegalArgumentException("N cannot be less or equal than 0\n");
+        }
+        worldSize = N;
+        numberOfOpened = 0;
+        world = new boolean[worldSize * worldSize]; //new boolean is false default
+        disjointSet = new WeightedQuickUnionUF(worldSize * worldSize + 2);
+        top = worldSize * worldSize;
+        bottom = worldSize * worldSize + 1;
+    }
+
+    public int locationCal(int row, int col){
+        return row * worldSize + col;
+    }
+    public void open(int row, int col) {    // open the site (row, col) if it is not open already
+        int loc = locationCal(row, col);
+        if (!world[loc]){
+            world[loc] = true;
+            numberOfOpened++;
+        }
+        //up. if the up block of the loc is opened, union the loc block and the block above
+        if (loc / worldSize - 1 >= 0 && world[loc - worldSize]){
+            disjointSet.union(loc, loc - worldSize);
+        }
+        //down
+        if (loc / worldSize + 1 < worldSize && world[loc + worldSize]){
+            disjointSet.union(loc, loc + worldSize);
+        }
+        //left
+        if (loc / worldSize - 1 >= 0 && world[loc - 1]){
+            disjointSet.union(loc, loc - 1);
+        }
+        //right
+        if (loc / worldSize - 1 < worldSize && world[loc + 1]){
+            disjointSet.union(loc, loc + 1);
+        }
+
+        //connect with the top?
+        if (row == 0){
+            disjointSet.union(loc, top);
+        }
+
+        //connect with the bottom?
+        if (row == worldSize - 1){
+            disjointSet.union(loc, bottom);
+        }
+    }
+
+    public boolean isOpen(int row, int col) {  // is the site (row, col) open?
+        int loc =locationCal(row, col);
+        return world[loc];
+    }
+    public boolean isFull(int row, int col) {  // is the site (row, col) full?
+        return disjointSet.connected(locationCal(row, col), top);
+    }
+    public int numberOfOpenSites() {           // number of open sites
+        return numberOfOpened;
+    }
+
+    public boolean percolates() {              // does the system percolate?
+        return disjointSet.connected(top, bottom);
+    }
+
+    public static void main(String[] args) {   // use for unit testing (not required)
+        System.out.println("Running main");
+    }
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,57 @@
 package hw2;
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
 public class PercolationStats {
+    private int T;
+    private double[] x;
 
+    private int[] generateUnrepeatableRandomNumber(int range){//generate a series of random numbers from 1-range
+        if (range <= 0){
+            throw new IllegalArgumentException("size should be > 0\n");
+        }
+        int[] source = new int[range];
+        for (int i = 0; i < range; i++){
+            source[i] = i;
+        }
+
+        int index, len = range;
+        for (int i = 0; i < range; i++){
+            index = StdRandom.uniform(len--);//uniform(N) generates integer in [0, N-1]
+            int temp = source[index];
+            source[index] = source[len];
+            source[len] = temp;
+        }
+        return source;
+    }
+    public PercolationStats(int N, int T, PercolationFactory pf) {   // perform T independent experiments on an N-by-N grid
+        if (N <= 0 || T <= 0){
+            throw new IllegalArgumentException("N and T should be > 0\n");
+        }
+        this.T = T;
+        x = new double[T];
+        for (int i = 0; i < T; i++){
+            Percolation world = pf.make(N);
+            int[] pos = generateUnrepeatableRandomNumber(N * N);
+            int index = 0;//to pick a random number in the pos[N*N] from 0
+            while(!world.percolates()){
+                int position = pos[index];
+                int row = position / N, col = position % N;
+                world.open(row, col);
+            }
+            x[i] = (double) world.numberOfOpenSites() / N * N;
+        }
+    }
+    public double mean() {             // sample mean of percolation threshold
+        return StdStats.mean(x);
+    }
+    public double stddev() {           // sample standard deviation of percolation threshold
+        return StdStats.stddev(x);
+    }
+    public double confidenceLow() {    // low endpoint of 95% confidence interval
+        return mean() - 1.96 * stddev() / Math.sqrt(T);
+    }
+    public double confidenceHigh() {   // high endpoint of 95% confidence interval
+        return mean() + 1.96 * stddev() / Math.sqrt(T);
+    }
 }
