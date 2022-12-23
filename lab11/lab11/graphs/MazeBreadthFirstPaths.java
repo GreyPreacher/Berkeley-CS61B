8c89470e013bbf901a7ef170933afaa7fe44a0cf
Alvin Zhang <zhangxiaoting320@gmail.com>
2022-12-23 19:17:21 +0800

    complete lab11

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,5 +1,8 @@
 package lab11.graphs;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  *  @author Josh Hug
  */
@@ -9,21 +12,49 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int source;
+    private int target;
+    private Maze maze;
+    private boolean isFound = false;
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
         // Add more variables here!
+        maze = m;
+        source = maze.xyTo1D(sourceX, sourceY);
+        target = maze.xyTo1D(targetX, targetY);
+        distTo[source] = 0;
+        edgeTo[source] = source;
     }
 
     /** Conducts a breadth first search of the maze starting at the source. */
-    private void bfs() {
+    private void bfs(int s) {
         // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(s);
+        marked[s] = true;
+        while(!queue.isEmpty()) {
+            int front = queue.remove();
+            if (front == target) {
+                isFound = true;
+                break;
+            }
+            for (int w: maze.adj(front)) {
+                if (!marked[w]) {
+                    marked[w] = true;
+                    distTo[w] = distTo[front] + 1;
+                    edgeTo[w] = front;
+                    announce();
+                    queue.add(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs(source);
     }
 }
 
--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,42 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private Maze maze;
+    private boolean isFould = false;
+    private int[] nodeTo;
 
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        nodeTo = new int[maze.N() * maze.N()];
     }
 
     @Override
     public void solve() {
         // TODO: Your code here!
+        dfs(-1, 0);
     }
 
     // Helper methods go here
+    private void dfs(int u, int v) {
+        marked[v] = true;
+        announce();
+        for (int w: maze.adj(v)) {
+            if (!marked[w]) {
+                marked[w] = true;
+                nodeTo[w] = v;
+                dfs(w, v);
+            } else if (w != v) {
+                edgeTo[w] = v;
+                announce();
+                for (int x = v; x != w; x = nodeTo[x]) {
+                    edgeTo[x] = nodeTo[x];
+                    announce();
+                }
+                isFould = true;
+            }
+            if (isFould) return;
+        }
+    }
 }
 
